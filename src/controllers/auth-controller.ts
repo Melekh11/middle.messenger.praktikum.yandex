b54e9d52ba5eb75b7ada4f038e2f7bc36176b689
Melekh11 <cktkutxd@gmail.com>
2022-08-13 00:48:49 +0300

    sprint-3 debug

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,59 +1,76 @@
-import { authApi, ISingIn, ISingUp } from "../api/auth-api";
-import { MyRouter, routs } from "../index";
-import { store } from "../utils/core/Store";
-import { chatsController } from "./chats-controller";
+import {authApi,  ISingIn, ISingUp} from "../api/auth-api";
+import {MyRouter, routs} from "../index";
+import {store} from "../utils/core/Store";
+import {chatsController} from "./chats-controller";
+
 
 class AuthController {
+
   public async signIn(data: ISingIn) {
     console.log("sign in controller");
-    return authApi
-      .signIn(data)
-      .then(() => {
-        authApi.userInfo().then(async (data) => {
-          console.log("setting user:", data.response);
-          await store.setState("user", data.response);
-          await chatsController.getAllChats().then(async (data) => {
-            await store.setState("chats", data.response);
-          });
-          MyRouter.go(routs.chatsPage);
-        });
-      })
-      .catch(() => {
-        console.log("error");
-        // authApi.logOut();
-        MyRouter.go(routs.errorPage);
-      });
+    return authApi.signIn(data)
+        .then( () => {
+          authApi.userInfo()
+              .then( async (data) => {
+                console.log("setting user:", data.response)
+                await store.setState("user", data.response);
+                await chatsController.getAllChats()
+                    .then(async (data) => {
+                      await store.setState("chats", data.response);
+                    })
+                MyRouter.go(routs.chatsPage);
+              })
+        })
+        .catch(() => {
+          console.log("error");
+          // authApi.logOut();
+          MyRouter.go(routs.errorPage);
+        })
   }
 
   public singUp(data: ISingUp) {
-    return authApi
-      .singUp(data)
-      .then(() => {
-        authApi.userInfo().then(async (data) => {
-          await store.setState("user", data.response);
-          await chatsController.getAllChats().then(async (data) => {
-            await store.setState("chats", data.response);
-          });
-          MyRouter.go(routs.chatsPage);
+    return authApi.singUp(data)
+        .then(() => {
+          authApi.userInfo()
+              .then(async (data) => {
+                await store.setState("user", data.response);
+                await store.setState("currentChat", false);
+                await chatsController.getAllChats()
+                    .then(async (data) => {
+                      await store.setState("chats", data.response);
+                    })
+                MyRouter.go(routs.chatsPage);
+              })
+        })
+        .catch(() => {
+          console.log("error");
+          MyRouter.go(routs.errorPage);
         });
-      })
-      .catch(() => {
-        console.log("error");
-        MyRouter.go(routs.errorPage);
-      });
   }
 
   public singOut() {
-    authApi.logOut().then(() => {
-      MyRouter.go(routs.signInPage);
-      store.setState("user", undefined);
-    });
+    authApi.logOut()
+        .then(() => {
+          console.log("doing logout");
+          store.setState("user", undefined);
+          // store.setState("chats", undefined);
+          store.setState("currentChat", false);
+          MyRouter.go(routs.signInPage);
+
+        })
   }
 
   public checkUser() {
-    return authApi.userInfo();
+    return authApi.userInfo()
+        .then((user) => {
+          store.setState("currentUser", user);
+        })
+        .catch(() => {
+          console.log("error")
+          MyRouter.go(routs.errorPage);
+        })
   }
 }
 
 const authController = new AuthController();
-export { authController };
+export {authController};
