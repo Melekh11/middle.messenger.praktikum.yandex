c6a4fa3b91fa1cf9c6d9e97b25a7fb2adc562159
Melekh11 <cktkutxd@gmail.com>
2022-08-12 22:14:28 +0300

    sprint-3 deploy

---
+++ src/helpers/checkers/authCheck.ts
@@ -0,0 +1,20 @@
+import { store } from "../../utils/core/Store";
+import { chatsController } from "../../controllers/chats-controller";
+import { authApi } from "../../api/auth-api";
+import { MyRouter, routs } from "../../index";
+
+export function checkAuth() {
+    authApi
+        .userInfo()
+        .then(async (data) => {
+            console.log("setting user:", data.response);
+            await store.setState("user", data.response);
+            await chatsController.getAllChats().then(async (data) => {
+                await store.setState("chats", data.response);
+            });
+            MyRouter.go(routs.chatsPage);
+        })
+        .catch(() => {
+            MyRouter.go(routs.signInPage);
+        });
+}
