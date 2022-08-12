58190aa8f139f860e8c9013a41ad7a9caf29afcb
Melekh11 <cktkutxd@gmail.com>
2022-08-13 00:43:16 +0300

    sprint-3 debug

--- src/components/dialog-field/dialog-field.ts
+++ src/components/dialog-field/dialog-field.ts
@@ -1,20 +1,20 @@
-import { Input } from "../input/input";
+import {Input} from "../input/input";
 import "./dialog-field.less";
 import dialogTemplate from "./dialog-field.pug";
-import { Block } from "../../utils/core/Block";
-import { connect } from "../../utils/core/HOC";
-import { Button } from "../button/button";
-import { store } from "../../utils/core/Store";
+import {Block} from "../../utils/core/Block";
+import {connect} from "../../utils/core/HOC";
+import {Button} from "../button/button";
+import {store} from "../../utils/core/Store";
 
 type DialogFieldProps = {
   currentChat: number;
   idChat: number;
-  dialogChosen: boolean;
+  dialogChosen: boolean
   inputMessage: Input;
   btnSendMessage: Button;
-};
+}
 
-class DialogField extends Block<DialogFieldProps> {
+class DialogField extends Block <DialogFieldProps> {
   constructor(props: DialogFieldProps) {
     super("div", {
       ...props,
@@ -27,27 +27,24 @@ class DialogField extends Block<DialogFieldProps> {
         inputCheckType: "chat",
       }),
       btnSendMessage: new Button({
-        className: "index-send-btn",
-        btnText: "→",
+        className: "index-send-btn", btnText: "→",
         events: {
           click: () => {
-            const textMessage = (
-              document.querySelector(".index-input-message") as HTMLInputElement
-            ).value;
+            const textMessage = (document.querySelector(".index-input-message") as HTMLInputElement).value;
             const socket = store.getState().socket;
-            socket.send(
-              JSON.stringify({
-                content: textMessage,
-                type: "message",
-              })
-            );
-          },
-        },
-      }),
-    });
+            socket.send(JSON.stringify({
+              content: textMessage,
+              type: 'message',
+            }));
+          }
+        }
+      })
+    })
+    ;
   }
 
   render() {
+    console.log(this.props);
     return this.compile(dialogTemplate, this.props);
   }
 }
@@ -55,16 +52,21 @@ class DialogField extends Block<DialogFieldProps> {
 function mapToDialog(store: any) {
   let isDialog: boolean = false;
   if (store.currentChat) {
+    console.log(store.currentChat);
     isDialog = true;
   }
+  let userId = null;
+  if (store.user){
+    userId = store.user.id
+  }
   return {
-    userId: store.user.id,
+    userId: userId,
     dialogChosen: isDialog,
-    currentChat: store.curruntChat,
-    messageFlow: store.currentDialog || [],
-  };
+    currentChat: store.curruntChat || null,
+    messageFlow: store.currentDialog || []
+  }
 }
 
 const con = connect(mapToDialog);
 const dialogField = con(DialogField);
-export { dialogField as DialogField };
+export {dialogField as DialogField};
