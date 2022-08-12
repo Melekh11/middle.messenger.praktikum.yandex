84bede67401a37db5a6480d6f9ef6522d8075b7f
Melekh11 <cktkutxd@gmail.com>
2022-08-12 22:15:11 +0300

    sprint-3 deploy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import "./index.less";
-import {Router} from "./utils/core/Router";
-import {ErrorPage} from "./pages/errors/error";
-import {ProfilePage} from "./pages/profile/profile";
-import {ChangeProfilePage} from "./pages/change-profile/changeProfile";
-import {ChangePasswordPage} from "./pages/change-password/change-password";
-import {SignInPage} from "./pages/sing-in/login";
-import {SignUpPage} from "./pages/sign-up/sing-up";
-import {ChatsPage} from "./pages/chats/chats";
-import {store} from "./utils/core/Store";
-import {authController} from "./controllers/auth-controller";
-
+import { Router } from "./utils/core/Router";
+import { ErrorPage } from "./pages/errors/error";
+import { ProfilePage } from "./pages/profile/profile";
+import { ChangeProfilePage } from "./pages/change-profile/changeProfile";
+import { ChangePasswordPage } from "./pages/change-password/change-password";
+import { SignInPage } from "./pages/sing-in/login";
+import { SignUpPage } from "./pages/sign-up/sing-up";
+import { ChatsPage } from "./pages/chats/chats";
+import { checkAuth } from "./helpers/checkers/authCheck";
 
 export enum routs {
     signUpPage = "/sign-up",
@@ -18,13 +16,12 @@ export enum routs {
     signInPage = "/sign-in",
     changePasswordPage = "/change-password",
     profilePage = "/profile",
-    errorPage = "/error"
+    errorPage = "/error",
 }
 
 // @ts-ignore какая-то странная ошибка Cannot redeclare exported variable 'MyRouter', в гугле ничего толкового не нашёл
 export const MyRouter = new Router();
-MyRouter
-    .use(routs.signUpPage, SignUpPage)
+MyRouter.use(routs.signUpPage, SignUpPage)
     .use(routs.profilePage, ProfilePage, {
         login: "unknown",
         shortName: "unknown",
@@ -36,13 +33,7 @@ MyRouter
     .use(routs.chatsPage, ChatsPage)
     .use(routs.signInPage, SignInPage)
     .use(routs.changePasswordPage, ChangePasswordPage)
-    .use(routs.errorPage, ErrorPage, {errorNumber: 404});
+    .use(routs.errorPage, ErrorPage, { errorNumber: 404 })
+    .start();
 
-
-if (store.getState().user) {
-    MyRouter.go(routs.profilePage);
-} else {
-    authController.singOut();
-    MyRouter.go(routs.signInPage);
-}
-MyRouter.start();
+checkAuth();
